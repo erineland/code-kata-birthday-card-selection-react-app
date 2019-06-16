fb20ab2e1bac2814473c3c517b4ac6faa9cd6d15
Erin Eland <erin@erineland.com>
2019-06-16 17:09:37 +0100

    Setting up React bootstrap via Webpack

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 
 const HelloWorld = () => <h1 data-testid="app-title">Hello Moonpig!</h1>;
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <Router>
       <Route exact path="/" component={HelloWorld} />
+      <Button>Hello React Bootstrap!</Button>
     </Router>
   )
 }
--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app.jsx';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 import styles from './scss/application.scss';
 
